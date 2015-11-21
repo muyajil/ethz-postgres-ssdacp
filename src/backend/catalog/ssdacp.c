ee6c8f8bb533a67e9a67a3bfca06587d4f48222f
Mohammed Ajil <muy@ajil.ch>
2015-11-21 12:49:31 +0100

    added ssdacp_restrict_and_check_grant

---
+++ src/backend/catalog/ssdacp.c
@@ -0,0 +1,17 @@
+
+
+static AclMode ssdacp_restrict_and_check_grant(bool is_grant, AclMode avail_goptions,
+						 bool all_privs, AclMode privileges,
+						 Oid objectId, Oid grantorId,
+						 AclObjectKind objkind, const char *objname,
+						 AttrNumber att_number, const char *colname);
+
+
+static AclMode
+ssdacp_restrict_and_check_grant(bool is_grant, AclMode avail_goptions, bool all_privs,
+						 AclMode privileges, Oid objectId, Oid grantorId,
+						 AclObjectKind objkind, const char *objname,
+						 AttrNumber att_number, const char *colname)
+{
+
+}

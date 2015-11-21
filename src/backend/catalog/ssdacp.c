237b3eaa26b59c66c04ae84c68f6f707696eda7d
Mohammed Ajil <muy@ajil.ch>
2015-11-21 16:59:00 +0100

    Add prototype & first implementation of ssdacp_RangeVarGetAndCheckCreationNamespace

--- src/backend/catalog/ssdacp.c
+++ src/backend/catalog/ssdacp.c
@@ -4,18 +4,29 @@
 #include "utils/acl.h"
 #include "access/attnum.h"
 
+//Definition of function that checks GRANT
 static AclMode ssdacp_restrict_and_check_grant(bool is_grant, AclMode avail_goptions,
 						 bool all_privs, AclMode privileges,
 						 Oid objectId, Oid grantorId,
 						 AclObjectKind objkind, const char *objname,
 						 AttrNumber att_number, const char *colname);
 
+//Definition of function that checks CREATE for relations
+Oid ssdacp_RangeVarGetAndCheckCreationNamespace(RangeVar *relation,
+									     LOCKMODE lockmode,
+									     Oid *existing_relation_id);
 
 static AclMode
 ssdacp_restrict_and_check_grant(bool is_grant, AclMode avail_goptions, bool all_privs,
 						 AclMode privileges, Oid objectId, Oid grantorId,
 						 AclObjectKind objkind, const char *objname,
 						 AttrNumber att_number, const char *colname)
+{
+}
+
+Oid ssdacp_RangeVarGetAndCheckCreationNamespace(RangeVar *relation,
+									     LOCKMODE lockmode,
+									     Oid *existing_relation_id)
 {
 
 }

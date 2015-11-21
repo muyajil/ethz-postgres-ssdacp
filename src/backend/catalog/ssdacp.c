2b5cc2e522e6ad58510475028859fd680be13cb7
Mohammed Ajil <muy@ajil.ch>
2015-11-21 13:15:18 +0100

    added includes for restrict_and_check_grant to ssdacp.c

--- src/backend/catalog/ssdacp.c
+++ src/backend/catalog/ssdacp.c
@@ -1,4 +1,8 @@
 #include "catalog/ssdacp.h"
+#include "postgres_ext.h"
+#include "nodes/parsenodes.h"
+#include "utils/acl.h"
+#include "access/attnum.h"
 
 static AclMode ssdacp_restrict_and_check_grant(bool is_grant, AclMode avail_goptions,
 						 bool all_privs, AclMode privileges,

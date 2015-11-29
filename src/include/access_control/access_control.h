810b1ee28b3558d0a66d5ef7e821671773cb0885
Mohammed Ajil <muy@ajil.ch>
2015-11-29 12:43:30 +0100

    added ac_grant_data

--- src/include/access_control/access_control.h
+++ src/include/access_control/access_control.h
@@ -7,19 +7,35 @@ typedef enum {INSERT, DELETE, SELECT, GRANT, REVOKE, CREATE_TRIGGER, CREATE_RELA
 
 #define SSDACP_ACTIVATE 1
 
+/* Data needed for deciding on CREATE queries */
 typedef struct ac_create_relation_data {
 	RangeVar *relation;
 	LOCKMODE lockmode;
 	Oid *existing_relation_id;
 } ac_create_relation_data;
 
+/* Data needed for deciding on GRANT queries */
+typedef struct ac_grant_data {
+	bool is_grant;
+	AclMode avail_goptions;
+	bool all_privs;
+	AclMode privileges;
+	Oid objectId;
+	Oid grantorId;
+	AclObjectKind objkind;
+	const char *objname;
+	AttrNumber att_number;
+	const char *colname;
+} ac_grant_data;
+
 typedef struct ac_decision_data {
 	bool trigger; /* Trigger Flag, TRUE if inside trigger */
 	command_type command; /* Flag that indicates the command that was used */
-	ac_create_relation_data create_data; /* If (command == CREATE_RELATION) we need to set this */
+	ac_create_relation_data create_data; /* If (command == CREATE_RELATION) this cannot be NULL */
+	ac_grant_data grant_data; /* If (command == GRANT || command == REVOKE) this cannot be NULL */
 } ac_decision_data;
 
-const struct AC_DECISION_DATA_DEFAULT {FALSE, NULL, NULL};
+const struct AC_DECISION_DATA_DEFAULT {FALSE, NULL, NULL, NULL};
 
 extern Oid ssdacp_RangeVarGetAndCheckCreationNamespace(RangeVar *relation,
 									     LOCKMODE lockmode,

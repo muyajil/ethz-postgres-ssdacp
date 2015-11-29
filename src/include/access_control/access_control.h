7b1b9ee8371ee54485b8e88d29fc1ea8317055ec
Mohammed Ajil <muy@ajil.ch>
2015-11-29 13:00:04 +0100

    added ac_create_trigger_data

--- src/include/access_control/access_control.h
+++ src/include/access_control/access_control.h
@@ -24,7 +24,7 @@ typedef struct ac_grant_data {
 	Oid grantorId;
 	AclObjectKind objkind;
 	const char *objname;
-	AttrNumber att_number;
+	AttrNumber att_number;constrrelid
 	const char *colname;
 } ac_grant_data;
 
@@ -34,12 +34,22 @@ typedef struct ac_nutility_data {
 	bool ereport_on_violation;
 } ac_nutility_data;
 
+/* Data needed for deciding on CREATE TRIGGER queries */
+typedef struct ac_create_trigger_data
+{
+	bool isInternal;
+	Relation rel;
+	Oid constrrelid;
+	AclResult *aclresult;
+} ac_create_trigger_data;
+
 typedef struct ac_decision_data {
 	bool trigger; /* Trigger Flag, TRUE if inside trigger */
 	command_type command; /* Flag that indicates the command that was used */
-	ac_create_relation_data create_data; /* If (command == CREATE_RELATION) this cannot be NULL */
+	ac_create_relation_data create_relation_data; /* If (command == CREATE_RELATION) this cannot be NULL */
 	ac_grant_data grant_data; /* If (command == GRANT || command == REVOKE) this cannot be NULL */
 	ac_nutility_data nutility_data; /* If (command == INSERT || command == DELETE || command == SELECT) this cannot be NULL */
+	ac_create_trigger_data create_trigger_data; /* If (command == CREATE_TRIGGER) this cannot be NULL */
 } ac_decision_data;
 
 const struct AC_DECISION_DATA_DEFAULT {FALSE, NULL, NULL, NULL, NULL};
@@ -63,7 +73,7 @@ extern AclMode ssdacp_restrict_and_check_grant(bool is_grant, AclMode avail_gopt
 						 AttrNumber att_number, const char *colname);
 
 /*
- * If allowed return address of created trigger
+ * If allowed do nothing
  * If not allowed aclcheck_error
  */
 extern ObjectAddress ssdacp_CreateTrigger(CreateTrigStmt *stmt, const char *queryString,

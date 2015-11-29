0fb75a556923bddf142dc6ed3b0aa1d9c86ad268
Mohammed Ajil <muy@ajil.ch>
2015-11-29 12:45:30 +0100

    added ac_nutility_data

--- src/include/access_control/access_control.h
+++ src/include/access_control/access_control.h
@@ -28,14 +28,21 @@ typedef struct ac_grant_data {
 	const char *colname;
 } ac_grant_data;
 
+/* Data needed for deciding on Non utility queries */
+typedef struct ac_nutility_data {
+	List *rangeTable;
+	bool ereport_on_violation;
+} ac_nutility_data;
+
 typedef struct ac_decision_data {
 	bool trigger; /* Trigger Flag, TRUE if inside trigger */
 	command_type command; /* Flag that indicates the command that was used */
 	ac_create_relation_data create_data; /* If (command == CREATE_RELATION) this cannot be NULL */
 	ac_grant_data grant_data; /* If (command == GRANT || command == REVOKE) this cannot be NULL */
+	ac_nutility_data nutility_data; /* If (command == INSERT || command == DELETE || command == SELECT) this cannot be NULL */
 } ac_decision_data;
 
-const struct AC_DECISION_DATA_DEFAULT {FALSE, NULL, NULL, NULL};
+const struct AC_DECISION_DATA_DEFAULT {FALSE, NULL, NULL, NULL, NULL};
 
 extern Oid ssdacp_RangeVarGetAndCheckCreationNamespace(RangeVar *relation,
 									     LOCKMODE lockmode,

08559fd9dd03f066ebad47f3fb855342e48511dd
Mohammed Ajil <muy@ajil.ch>
2016-03-03 19:36:46 +0100

    wrong variable name

--- src/backend/tcop/postgres.c
+++ src/backend/tcop/postgres.c
@@ -675,7 +675,7 @@ pg_analyze_and_rewrite(Node *parsetree, const char *query_string,
 	// Push to the stack
 	context.user = GetSessionUserId();
 	context.invoker = GetUserId();
-	context.query = parsed_query;
+	context.query = query;
 
 	ac_context_push(&context, &context_stack);
 
